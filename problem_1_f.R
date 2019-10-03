47c190dfaff6f0510afd72dc4e156cc40e978b26
karolhb <karolhb@stud.ntnu.no>
2019-10-03 12:52:20 +0200

    fix bugs in problem 1 f

--- problem_1_f.R
+++ problem_1_f.R
@@ -1,32 +1,30 @@
-# total population
-t = 1000
+# number of simulations
+M = 1000
 
-# initial population in each state
-Sn = 950
-In = 50
-Rn = 0
+# initialize lists of a fixed (great enough) length
+highestNumberVector = vector('numeric', length = M)
+highestIndexVector = vector('numeric', length = M)
 
-# initialize parameters
-N = 200
-beta = 0.5*In/t
-gamma = 0.2
-P = matrix(c(1-beta, beta, 0, 0, 1-gamma, gamma, 0, 0, 1), nrow = 3,byrow=TRUE)
-
-# y is an array to keep the results
-y = matrix('numeric', nrow = 3, ncol = 0)
-y = cbind(y, c(Sn, In, Rn))
-
-
-#Initialize lists of a fixed(great enough) length
-highestNumberVector<-list(1000)
-highestIndexVector<-list(1000)
-
-for (k in 1:100){
-  #initialize the highest number and the corresponding index as 0 and 1,
-  #respectively at each realization.
+for (k in 1:M){
+  # total population
+  t = 1000
   
-  highestNumber <-0
-  highestIndex <- 1
+  # initial population in each state
+  Sn = 950
+  In = 50
+  Rn = 0
+  
+  # initialize parameters
+  N = 200
+  beta = 0.5*In/t
+  gamma = 0.2
+  P = matrix(c(1-beta, beta, 0, 0, 1-gamma, gamma, 0, 0, 1), nrow = 3,byrow=TRUE)
+  
+  # initialize the highest number and the corresponding index as 0 and 1,
+  # respectively at each realization.
+  
+  highestNumber = 0
+  highestIndex = 1
   for (n in 1:N) {
     InInc = min(Sn, rbinom(1, Sn, beta))
     RnInc = min(In, rbinom(1,In,gamma))
@@ -36,26 +34,28 @@ for (k in 1:100){
     Rn = Rn + RnInc
     In = In - RnInc
     
-    # add result to array
-    y = cbind(y, c(Sn, In, Rn))
-    
     # update beta and matrix
     beta = 0.5*In/t
     P = matrix(c(1-beta, beta, 0, 0, 1-gamma, gamma, 0, 0, 1), nrow = 3,byrow=TRUE)
     
-    if( y[2][n] >= highestNumber ){
-      highestNumber <- y[2][n]
-      highestIndex <-n
+    if( In >= highestNumber ){
+      highestNumber = In
+      highestIndex = n
     }
   }
-  #Append the maximum and the index to a vector
-  #Reinitialize both maximum and the index as in 1)
-  highestNumberVector[k]<-highestNumber
-  highestIndexVector[k]<-highestIndex
+  # append the maximum and the index to a vector
+  # reinitialize both maximum and the index as in 1)
+  highestNumberVector[k] = highestNumber
+  highestIndexVector[k] = highestIndex
 }
 
-#Take the average of the values of highestNumberVector and highestIndexVector
-# Initialize to highestExpected and highestIndex respectively 
+# estimated expected maximum number of infected individuals
+# calculate the average of the maximum from the M simulations
+expMaxInf = sum(highestNumberVector)/M
+cat("Estimated expected maximum number of infected individuals: ", expMaxInf)
 
+# estimated expected time at which the maximum number of infected individuals is reached
+# calculate the average of the index of the maximum from the M simulations
+expMaxTime = sum(highestIndexVector)/M
+cat("\nEstimated expected time of maximum number of infected individuals: ", expMaxTime)
 
-print(highestNumberVector)
